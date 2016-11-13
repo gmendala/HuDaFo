3bc3e598fbfd10493f72bab7eb71ed53d15217e2
Grzegorz Mendala <lt.maxon@gmail.com>
2016-11-13 16:03:28 +0100

    Documentation for GeneratorBase
    Change-Id: I8a86abf98c212bfcc8b3f3297713a7d8083705ba

--- hudafo/hudafo-api/hudafo-core/src/main/java/com/gmitdesign/base/GeneratorBase.java
+++ hudafo/hudafo-api/hudafo-core/src/main/java/com/gmitdesign/base/GeneratorBase.java
@@ -1,6 +1,7 @@
 package com.gmitdesign.base;
 
 import java.util.List;
+import com.gmitdesign.base.GeneratorOutput;
 
 /**
  * Base for generator classes. You should use it by inheriting it into more complex and
@@ -12,29 +13,64 @@ import java.util.List;
  */
 public abstract class GeneratorBase {
 
+	/** Output that will be used for generated data **/
 	private GeneratorOutput output;
+	/** Utility name of the implemented generator. **/
 	private String name;
 	
+	/**
+	 * Constructor with defined output interface.
+	 * @param output - this is an interface designed to take generated data and 
+	 * dispatch it to designed destination. Destination can be defined as console, file,
+	 * database and others.
+	 */
 	public GeneratorBase(GeneratorOutput output){
 		this.output = output;
 		
 	}
 	
+	/**
+	 * Constructor with defined output interface and implementation name.
+	 * @param output - this is an interface designed to take generated data and 
+	 * dispatch it to designed destination. Destination can be defined as console, file,
+	 * database and others.
+	 * @param name - specific name used to identify the exact implementation of the generator.
+	 */
 	public GeneratorBase(GeneratorOutput output, String name){
 		this.output = output;
 		this.name = name;		
 	}
 	
+	/**
+	 * This is the main method for data generation. It creates output as a list of T.
+	 * In this method, you should write down everything that generator should do to
+	 * return a list of generated data. You ssould remember, that in implementation this will create 
+	 * raw list of data (if you need it), but generators are designed to return data to
+	 * their defined output, so we recommend to use generate() method.
+	 * @return A list of T after the process of data generation.
+	 */
 	public abstract <T> List<T> processTheData();
 	
+	/**
+	 * Main method of this generator. Invoking it will generate data and send it to defined output.
+	 * You should use it as an implementation method for generating data and sending it to the output.
+	 */
 	public void generate(){
 		output.dispatchGeneratedData(processTheData());
 	}
 
+	/**
+	 * Gets the name defined for implementation of this generator.
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the name of this generator.
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
